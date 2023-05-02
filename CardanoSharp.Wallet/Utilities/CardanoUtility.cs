c2a207b3c14d3a01595ecc6d4518728a36183acf
Nicholas Maselli <maselli.nicholas@gmail.com>
2023-05-02 12:29:18 -0400

    slot utility

--- CardanoSharp.Wallet/Utilities/CardanoUtility.cs
+++ CardanoSharp.Wallet/Utilities/CardanoUtility.cs
@@ -11,23 +11,4 @@ public static class CardanoUtility
     public static double priceStep = 0.0000721;
     public static long maxTxExMem = 14000000;
     public static long maxTxExSteps = 10000000000;
-
-    // Slot height at the start of the Shelly Era (Where 1 slot becomes 1 second)
-    public static long ShellySlot = 4924800;
-
-    // UNIX time at the start of the Shelly Era
-    public static long ShellyUNIX = 1596491091;
-
-    public static long GetSlotFromUnixTIme(long unixTime)
-    {
-        return unixTime - ShellyUNIX + ShellySlot;
-    }
-
-    public static DateTime GetUTCTimeFromSlot(long slot)
-    {
-        long unixTime = ShellyUNIX + (slot - ShellySlot);
-        DateTime posixEpoch = new DateTime(1970, 1, 1, 0, 0, 0, DateTimeKind.Utc);
-        DateTime unixDatetime = posixEpoch.AddSeconds(unixTime);
-        return unixDatetime;
-    }
 }
