02b2937948ef9d886e418aba0abd2dce0577cd68
Nicholas Maselli <maselli.nicholas@gmail.com>
2023-10-11 19:39:54 -0400

    cardano utility update

--- CardanoSharp.Wallet/Utilities/CardanoUtility.cs
+++ CardanoSharp.Wallet/Utilities/CardanoUtility.cs
@@ -2,13 +2,19 @@ namespace CardanoSharp.Wallet.Utilities;
 
 public static class CardanoUtility
 {
-    public static long adaToLovelace = 1000000;
-    public static double lovelaceToAda = 0.000001;
-    public static long adaOnlyMinUtxo = 1000000;
+    // Ada PolicyId and Asset Name
+    public const string adaFullAssetName = "";
+    public const string adaPolicyId = "";
+    public const string adaAssetName = "";
+
+    // Conversion Values
+    public const long adaToLovelace = 1000000;
+    public const double lovelaceToAda = 0.000001;
+    public const long adaOnlyMinUtxo = 1000000;
 
     // Protocol Parameters
-    public static double priceMem = 0.0577;
-    public static double priceStep = 0.0000721;
-    public static long maxTxExMem = 14000000;
-    public static long maxTxExSteps = 10000000000;
+    public const double priceMem = 0.0577;
+    public const double priceStep = 0.0000721;
+    public const long maxTxExMem = 14000000;
+    public const long maxTxExSteps = 10000000000;
 }
