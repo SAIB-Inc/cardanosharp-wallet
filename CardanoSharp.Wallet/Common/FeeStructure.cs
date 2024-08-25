75b2c8201b29f5f08d8469c1d25a72f2da1e0fb9
Clark Alesna <clark_alesna@hotmail.com>
2024-08-25 09:58:02 +0800

    feat: updated fee calculate to include Reference Script Fee formula

--- CardanoSharp.Wallet/Common/FeeStructure.cs
+++ CardanoSharp.Wallet/Common/FeeStructure.cs
@@ -2,11 +2,13 @@
 
 public static class FeeStructure
 {
-    // Constants taken from protocol params as of mainnet epoch 345
     public const uint Coefficient = 44;
     public const uint Constant = 155381;
     public const ulong MaxTxExMem = 14000000;
     public const ulong MaxTxExSteps = 10000000000;
     public const double PriceMem = 0.0577;
     public const double PriceStep = 0.0000721;
+    public const uint RefScriptBase = 15;
+    public const uint RefScriptRange = 25600;
+    public const double RefScriptMultiplier = 1.2;
 }
